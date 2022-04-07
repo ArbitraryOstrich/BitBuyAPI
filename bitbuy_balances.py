a6fba4783e6bc6b1227de8371e26eabe6af4502f
UpbeatOctopus <mason.thomas.kevin@gmail.com>
2022-04-07 17:30:48 -0400

    init

---
+++ bitbuy_balances.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import hmac
+import hashlib
+import requests
+import base64
+import time
+
+
+### Just a file in the local dir that contains
+# Pub = ""
+# Priv = ""
+import secrets
+
+
+stamp = int(round(time.time() * 1000))
+
+# api_address = f"https://partner.bcm.exchange/api/v1/coins?apikey={Pub}&stamp={stamp}"
+# json_data = f'{{\"path\":\"/api/v1/coins\",\"content-length\":-1,\"query\":\"apikey={Pub}&stamp={stamp}\"}}'
+
+
+api_address = (
+    f"https://partner.bcm.exchange/api/v1/wallets?apikey={secrets.Pub}&stamp={stamp}"
+)
+json_data = f'{{"path":"/api/v1/wallets","content-length":-1,"query":"apikey={secrets.Pub}&stamp={stamp}"}}'
+
+
+# https://docs.python.org/3/library/stdtypes.html?highlight=bytes#bytes
+hmac_msg = bytes(str(json_data), "UTF-8")
+hmac_key = bytes(secrets.Priv, "UTF-8")
+# https://docs.python.org/3/library/hmac.html?highlight=hmac#module-hmac
+hmac_val = hmac.new(key=hmac_key, msg=hmac_msg, digestmod=hashlib.sha256).digest()
+# https://docs.python.org/3/library/base64.html?highlight=base64#module-base64
+sig = base64.b64encode(hmac_val).decode()
+
+print(f"Hmac_key: {hmac_key}")
+print(f"hmac_msg: {hmac_msg}")
+print(f"Hmac_val: {hmac_val}")
+print(f"Base64Sig: {sig}\n\n")
+
+headers = {"Content-Type": "application/json", "signature": sig}
+response = requests.request("GET", api_address, headers=headers)
+response.json()
+response.status_code
+
+
+for a in response.json():
+    if a["balance"] != 0:
+        print(a)
